54d0f1566f82b16f1011ebed1fcf275038790453
srijaygusain <78269469+srijaygusain@users.noreply.github.com>
2022-10-30 15:50:17 +0530

    Create customhash.cpp

---
+++ customhash.cpp
@@ -0,0 +1,7 @@
+struct custom_hash {
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        x ^= FIXED_RANDOM;
+        return x ^ (x >> 16);
+    }
+};
