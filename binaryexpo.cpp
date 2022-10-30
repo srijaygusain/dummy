a8e2a39f52aba013d96c8be49b8f526287876530
srijaygusain <78269469+srijaygusain@users.noreply.github.com>
2022-10-30 15:19:56 +0530

    Create binaryexpo.cpp

---
+++ binaryexpo.cpp
@@ -0,0 +1,17 @@
+int powmod(long long x,int n,int mod)
+{	
+    x%=mod;
+    if(x==0)
+    return 0;
+    long long res=1;
+    while(n)
+    {
+      if(n&1)
+      res=res*x%mod;
+      n/=2;
+      x=x*x%mod;
+    }
+    if(res<0)
+    res+=mod;
+    return res;
+}
